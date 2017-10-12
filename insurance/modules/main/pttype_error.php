f9a176a48b0e8a6f9c9dfdfd9fc1abb7fcbd9990
DESKTOP-HAQSS2E\yeawnote <yeawyow@gmail.com>
2017-10-12 14:08:19 +0700

    ล่าสุด

--- insurance/modules/main/pttype_error.php
+++ insurance/modules/main/pttype_error.php
@@ -1,77 +1,88 @@
 <section class="content-header">
-    <h1>
+      <h1>
         PTTYPE ERROR
         <small>Control panel</small>
-    </h1>
-    <ol class="breadcrumb">
+      </h1>
+      <ol class="breadcrumb">
         <li><a href="./"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active"></li>
-    </ol>
-</section>
+      </ol>
+    </section>
 
-<div class="col-xs-12">
-    <div class="box">
-        <div class="box-header">
-            <h3 class="box-title"></h3>
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title"></h3>
 
-            <div class="box-tools">
+              <div class="box-tools">
                 <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
 
-                    <div class="input-group-btn">
-                        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                    </div>
+                  <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body table-responsive no-padding">
-            <table class="table table-hover">
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
                 <tr>
-                    <th>ลำดับ</th>
-                   
+                  <th>ลำดับ</th>
+                  <th>HN</th>
+                  <th>ชื่อ-สกุล</th>
+                  <th>CID</th>
+                  <th>วันที่รับบริการ</th>
+                  <th>สถานบริการ</th>
+                  <th>ผู้ส่งตรวจ</th>
                 </tr>
-                <tbody id="show-list-data ">
-                   
+                <tbody class="show-list-data ">
+                       <tr class=" list-data">
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
                 </tbody>
-            </table>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
         </div>
-        <!-- /.box-body -->
-    </div>
-    <!-- /.box -->
-</div>
 
-<script type="text/javascript">
-    
-    var dataList = {}
-    $(function () {
-        dataList.getList = (function () {
-            
-            var url = "modules/main/pttype_error_data.php"; // ไฟล์ที่ต้องการรับค้า
-            var dataSet = {action: 'list'}; // กำหนดชื่อและค่าที่ต้องการส่ง
-             //var rowData = $(".list-data").clone(true);
-         
-            $.post(url, dataSet, function (response) {
-                // alert("แจ้งเเมื่อทำการส่งข้อมูลเรียบร้อยแล้ว");
-                console.log(response);
-               // var rowListData = "";
-                var myJSON = [{     "user_id": "1",     "user_name": "User1",     "user_age": "20" }, {     "user_id": "2",     "user_name": "User2",     "user_age": "21"              }, {     "user_id": "3",     "user_name": "User3",     "user_age": "24" }, {     "user_id": "4",     "user_name": "User4",     "user_age":              "34" }, {     "user_id": "5",     "user_name": "User5",     "user_age": "27" }];
-                // $('.show-list-data tbody').empty();
-                 var obj = $.parseJSON(response);
-               
-                $.each(obj, function (key,val) {
-                    var tr = "<tr>";
-                  tr = tr + "<td>"+val["hospmain"]+"</td>";
-                  tr = tr + "</tr>";
-                  $('.show-list-data > tbody:last').append(tr);
-                }); // end loop	
-            });
-           
-        });
-       
-        dataList.getList(0, true);
-       
-      //setInterval(dataList.getList, 10000);   // 1000 = 1 second   
+      <script type="text/javascript">
+var dataList = {}
+$(function(){
+   dataList.getList =(function(){
+        var url="modules/main/pttype_error_data.php"; // ไฟล์ที่ต้องการรับค้า
+        var dataSet={ action:'list' }; // กำหนดชื่อและค่าที่ต้องการส่ง
+        var rowData = $(".list-data").clone(true);
+        $.post(url,dataSet,function(response){
+           // alert("แจ้งเเมื่อทำการส่งข้อมูลเรียบร้อยแล้ว");
+           console.log(response);
+           var rowListData="";
+          $.each(response.data,function(i){
+					rowListData = "";
+					rowListData+="<tr class=\"list-data\">";
+					rowListData+=$(
+                                        rowData.find("td:eq(0)").text(response.data[i].no).end()
+					.find("td:eq(1)").text(response.data[i].hn).end()
+					.find("td:eq(2)").text(response.data[i].fullname).end()
+                                        .find("td:eq(3)").text(response.data[i].cid).end()
+                                        .find("td:eq(4)").text(response.data[i].vstdate).end()
+                                        .find("td:eq(5)").text(response.data[i].hospmain +" " + response.data[i].hosname).end()
+					  .find("td:eq(6)").text(response.data[i].name_staff).end()
+					).html();	
+					rowListData+="</tr>";
+					$(".show-list-data").append(rowListData);
+				}); // end loop	
+         });
     });
+    dataList.getList(0,true);
+});
 </script>
-
+	
--- insurance/modules/main/pttype_error_data.php
+++ insurance/modules/main/pttype_error_data.php
@@ -7,31 +7,27 @@ if (isset($_POST['action']) && $_POST['action'] == "list") {
     include_once '../../../lib/config.inc.php';
     $Db5 = new MySqlConn5;
 
-    $sql = "SELECT CONCAT(hp.hosptype,'',hp.name) AS hosname,pat.cid,CONCAT(pat.pname,pat.fname,' ',pat.lname) AS fullname,vn.vstdate,vn.hn,vn.vn,vn.pttype,vn.hospmain FROM vn_stat vn
+    $sql = "SELECT pat.cid, CONCAT(hp.hosptype,'',hp.name) AS hosname,CONCAT(pat.pname,pat.fname,' ',pat.lname) AS fullname,vn.vstdate,vn.hn,vn.vn,vn.pttype,vn.hospmain,ou.name AS name_staff FROM vn_stat vn
 Left OUTER JOIN pttype pt ON pt.pttype=vn.pttype
 LEFT OUTER JOIN patient pat ON pat.hn=vn.hn
 LEFT OUTER JOIN hospcode hp ON hp.hospcode=vn.hospmain
+INNER JOIN ovst ov ON ov.vn=vn.vn
+LEFT OUTER JOIN opduser ou ON ou.loginname=ov.staff
 WHERE vn.vstdate BETWEEN '2017-08-01' AND curdate() AND pt.hipdata_code IN('UCS','WELL') AND vn.pttype NOT IN ('13','15','98','58','40','99','17') AND vn.hospmain NOT IN('11098')";
 
     $result = $Db5->query($sql, '');
-    
-             
-             
-        foreach ($result as $row ) {
-            $data[] = $row ;
-        }
-             
-                echo json_encode($row);   
-    
-  /*  foreach ($result AS $row) {
+
+    foreach ($result AS $row) {
+        $no++;
         $json_data['data'][] = array(
+            "no" => $no,
             "hn" => $row['hn'],
             "fullname" => $row['fullname'],
-            "cid" => $row['cid'],
-            "vstdate" => $row['vstdate'],
-            "pttype" => $row['pttype'],
-            "hospmain" => $row['hospmain'],
-            "hosname" => $row['hosname']
+             "cid"=>$row['cid'],
+             "vstdate"=>$row['vstdate'],
+             "hospmain"=>$row['hospmain'],
+             "hosname"=>$row['hosname'],
+              "name_staff"=>$row['name_staff']
         );
     }
     if (isset($json_data)) {
@@ -41,5 +37,5 @@ WHERE vn.vstdate BETWEEN '2017-08-01' AND curdate() AND pt.hipdata_code IN('UCS'
         } else {
             echo $json;
         }
-    }*/
+    }
 }
