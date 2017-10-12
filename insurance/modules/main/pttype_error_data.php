90392bdfd91e03bb014697ef48dad4a9f0557a12
DESKTOP-HAQSS2E\yeawnote <yeawyow@gmail.com>
2017-10-12 14:18:37 +0700

    2

--- insurance/modules/main/pttype_error_data.php
+++ insurance/modules/main/pttype_error_data.php
@@ -13,7 +13,7 @@ LEFT OUTER JOIN patient pat ON pat.hn=vn.hn
 LEFT OUTER JOIN hospcode hp ON hp.hospcode=vn.hospmain
 INNER JOIN ovst ov ON ov.vn=vn.vn
 LEFT OUTER JOIN opduser ou ON ou.loginname=ov.staff
-WHERE vn.vstdate BETWEEN '2017-08-01' AND curdate() AND pt.hipdata_code IN('UCS','WELL') AND vn.pttype NOT IN ('13','15','98','58','40','99','17') AND vn.hospmain NOT IN('11098')";
+WHERE vn.vstdate BETWEEN '2016-08-01' AND curdate() AND pt.hipdata_code IN('UCS','WELL') AND vn.pttype NOT IN ('13','15','98','58','40','99','17') AND vn.hospmain NOT IN('11098')";
 
     $result = $Db5->query($sql, '');
 
